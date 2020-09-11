e178f2cced9cfc3cf88fd6aad282f20722add677
ririn nhidayah <nhidayahr68@gmail.com>
2020-09-11 11:21:44 +0700

    no message

---
+++ toko_online.sql
@@ -0,0 +1,322 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 02, 2020 at 10:49 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.3.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `toko_online`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `berita`
+--
+
+CREATE TABLE `berita` (
+  `id_berita` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `jenis_berita` varchar(200) NOT NULL,
+  `judul_berita` varchar(200) NOT NULL,
+  `slug_berita` varchar(200) NOT NULL,
+  `keywords` text DEFAULT NULL,
+  `status_berita` varchar(200) NOT NULL,
+  `keterangan` text NOT NULL,
+  `gambar` varchar(200) NOT NULL,
+  `tanggal_post` datetime NOT NULL,
+  `tanggal_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gambar`
+--
+
+CREATE TABLE `gambar` (
+  `id_gambar` int(11) NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `judul_gambar` varchar(200) DEFAULT NULL,
+  `gambar` varchar(200) NOT NULL,
+  `tanggal_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `header_transaksi`
+--
+
+CREATE TABLE `header_transaksi` (
+  `id_header_transaksi` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_pelanggan` int(11) NOT NULL,
+  `kode_transaksi` varchar(200) DEFAULT NULL,
+  `tanggal_transaksi` datetime NOT NULL,
+  `jumlah_transaksi` int(11) NOT NULL,
+  `status_bayar` varchar(200) NOT NULL,
+  `jumlah_bayar` int(11) DEFAULT NULL,
+  `rekening_pembayaran` varchar(200) DEFAULT NULL,
+  `rekening_pelanggan` varchar(200) DEFAULT NULL,
+  `bukti_bayar` varchar(200) DEFAULT NULL,
+  `tanggal_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `slug_kategori` varchar(200) NOT NULL,
+  `nama_kategori` varchar(200) NOT NULL,
+  `urutan` int(11) NOT NULL,
+  `tanggal_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `slug_kategori`, `nama_kategori`, `urutan`, `tanggal_update`) VALUES
+(1, 'buku', 'buku', 1, '2020-08-29 06:14:22'),
+(2, 'pensil', 'pensil', 2, '2020-08-29 06:14:22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `konfigurasi`
+--
+
+CREATE TABLE `konfigurasi` (
+  `id_konfigurasi` int(11) NOT NULL,
+  `nama_web` varchar(200) NOT NULL,
+  `tagline` varchar(200) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `website` varchar(200) DEFAULT NULL,
+  `keywords` text DEFAULT NULL,
+  `metatext` text DEFAULT NULL,
+  `telepon` varchar(200) DEFAULT NULL,
+  `alamat` varchar(200) DEFAULT NULL,
+  `facebook` varchar(200) DEFAULT NULL,
+  `instagram` varchar(200) DEFAULT NULL,
+  `deskripsi` text DEFAULT NULL,
+  `logo` varchar(200) DEFAULT NULL,
+  `icon` varchar(200) DEFAULT NULL,
+  `rekening_pembayaran` varchar(200) DEFAULT NULL,
+  `tanggal_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `id_pelanggan` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `status_pelanggan` varchar(200) NOT NULL,
+  `nama_pelanggan` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `telp` varchar(200) DEFAULT NULL,
+  `alamat` varchar(100) DEFAULT NULL,
+  `tanggal_daftar` datetime NOT NULL,
+  `tanggal_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `id_produk` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `kode_produk` varchar(200) NOT NULL,
+  `nama_produk` varchar(200) NOT NULL,
+  `slug_produk` varchar(200) NOT NULL,
+  `keterangan` text NOT NULL,
+  `keywords` text DEFAULT NULL,
+  `harga` int(11) NOT NULL,
+  `stok` int(11) DEFAULT NULL,
+  `gambar` varchar(200) NOT NULL,
+  `berat` float DEFAULT NULL,
+  `ukuran` varchar(200) DEFAULT NULL,
+  `status_produk` varchar(200) NOT NULL,
+  `tanggal_post` datetime NOT NULL,
+  `tanggal_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_pelanggan` int(11) NOT NULL,
+  `code_transaksi` varchar(200) NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `total_harga` int(11) NOT NULL,
+  `tanggal_transaksi` datetime NOT NULL,
+  `tanggal_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `akses_level` varchar(200) NOT NULL,
+  `tanggal_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id_user`, `nama`, `email`, `username`, `password`, `akses_level`, `tanggal_update`) VALUES
+(1, 'Ririn ', 'ririnnurulhidayah@gmail.com', 'Ririnn', '12345678', 'Admin', '2020-09-02 07:44:35');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `berita`
+--
+ALTER TABLE `berita`
+  ADD PRIMARY KEY (`id_berita`);
+
+--
+-- Indexes for table `gambar`
+--
+ALTER TABLE `gambar`
+  ADD PRIMARY KEY (`id_gambar`);
+
+--
+-- Indexes for table `header_transaksi`
+--
+ALTER TABLE `header_transaksi`
+  ADD PRIMARY KEY (`id_header_transaksi`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `konfigurasi`
+--
+ALTER TABLE `konfigurasi`
+  ADD PRIMARY KEY (`id_konfigurasi`);
+
+--
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id_pelanggan`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id_produk`),
+  ADD UNIQUE KEY `kode_produk` (`kode_produk`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `gambar`
+--
+ALTER TABLE `gambar`
+  MODIFY `id_gambar` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `header_transaksi`
+--
+ALTER TABLE `header_transaksi`
+  MODIFY `id_header_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `konfigurasi`
+--
+ALTER TABLE `konfigurasi`
+  MODIFY `id_konfigurasi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
